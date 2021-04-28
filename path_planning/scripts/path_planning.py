b94ce1e5fb61a70dff7515bb4b677e0d711592a3
Moritz Böker <moritz.boeker@gmx.de>
2021-04-28 10:40:26 -0700

    Now the angle of the robot to the center line of the corn row is being determined correctly

--- path_planning/scripts/path_planning.py
+++ path_planning/scripts/path_planning.py
@@ -9,44 +9,106 @@ from tf.transformations import quaternion_from_euler
 
 import numpy as np
 import matplotlib.pyplot as plt
+# from scipy.cluster.hierarchy import fclusterdata
 
 def scan2cart(scan, max_range=30.0):
     angles = np.linspace(scan.angle_min, scan.angle_max, len(scan.ranges))
-    ignore_ranges = np.array(scan.ranges) > max_range
     x = scan.ranges*np.cos(angles)
     y = scan.ranges*np.sin(angles)
     scan_cart = np.array([x, y])
-    scan_cart[:, ignore_ranges] = None
+    scan_cart = np.transpose(scan_cart)
+    # ignore_ranges = np.array(scan.ranges) > max_range
+    # scan_cart[ignore_ranges, :] = None
+    allow_ranges = np.array(scan.ranges) <= max_range
+    scan_cart = scan_cart[allow_ranges, :]
     return scan_cart
 
+
+
+
 def laser_callback(scan):
+    global alpha_old
+    global alpha_new
     if state == "state_in_row":
-        global angle
-        global row_width
+        # convert scan from polar into cartesian coordinates
+        scan_cart = scan2cart(scan, max_range=30.0)
 
-        angle_increment = scan.angle_increment
-        angle_outer_limit_curr = scan.angle_max
-        angle_outer_limit_targ = np.radians(130)
-        angle_inner_limit_targ = np.radians(20)
-        idx_ranges_outer = int(np.round((angle_outer_limit_curr - angle_outer_limit_targ) / angle_increment))
-        idx_ranges_inner = int(np.round((angle_outer_limit_curr - angle_inner_limit_targ) / angle_increment))
+        # determine euclidean distance between each subsequent scan point
+        x_dist_scan = np.diff(scan_cart[:, 0])
+        y_dist_scan = np.diff(scan_cart[:, 1])
+        eucl_dist = np.sqrt(x_dist_scan**2 + y_dist_scan**2)
+        eucl_dist = np.insert(eucl_dist, 0, 0, axis=0)
         
-        scan_cart = scan2cart(scan, max_range=row_width)
-        scan_left = scan_cart[:, -idx_ranges_inner:-idx_ranges_outer]
-        scan_right = scan_cart[:, idx_ranges_outer:idx_ranges_inner]
+        # # plot histogram of x, y and euclidean distance
+        # n_bins = 10
+        # fig, axs = plt.subplots(1, 3, sharey=True, tight_layout=True)
+        # axs[0].hist(x_dist_scan, bins=n_bins)
+        # axs[1].hist(y_dist_scan, bins=n_bins)
+        # axs[2].hist(eucl_dist, bins=n_bins)
+        # plt.show(block=True)
 
-        mean_left = np.nanmean(scan_left[1, :])
-        mean_right = np.nanmean(scan_right[1, :])
+        # determine which scan points belong to one and the same plant and split scan accordingly
+        idx_next_plant = np.where(eucl_dist > 0.05)
+        split_plants = np.vsplit(scan_cart, idx_next_plant[0])
 
-        offset = mean_right + mean_left
-        angle = - offset / (mean_left + mean_left)    
+        # determine mean position of all plants having at least n_points of scan points
+        n_points = 3
+        plants_all = []
+        for plant in split_plants:
+            if plant.shape[0] >= n_points:
+                mean_position = np.nanmean(plant, axis=0)
+                plants_all.append(mean_position)
+        plants_all = np.array(plants_all)
 
-        # # plot xy-graph
-        # scan_extract = np.concatenate((scan_right, scan_left), axis=1)
-        # print("scan_extract", scan_extract.shape)
+        # # plot xy-graph of cartesian scan and mean position of detected plants
         # plt.figure()
-        # plt.plot(scan_extract[0,:], scan_extract[1,:], "ob")
-        # plt.show()
+        # plt.plot(scan_cart[:,0], scan_cart[:,1], "ob", label="cartesian laser scan")
+        # plt.plot(plants_all[:,0], plants_all[:,1], "xr", label="averaged plant position with at least 3 laser points")
+        # plt.legend(loc='lower left')
+        # plt.show(block=True)
+
+        # calculate line angles from one plant to another
+        x_dist_plant = np.diff(plants_all[:, 0])
+        y_dist_plant = np.diff(plants_all[:, 1])
+        alphas = np.arctan2(y_dist_plant, x_dist_plant)
+        # change perspective plants to robot --> robot to plants
+        alphas = -alphas
+        # Modify angles so that 
+        # e.g. [   9  125   17   -2   75 -176 -175   57 -164] --> [   9  125   17   -2   75    3    4   57 -164]
+        alphas = np.where(alphas < np.radians(-170), alphas + np.pi, alphas)
+        alphas = np.where(alphas > np.radians(170), alphas - np.pi, alphas)
+
+        # determine the histogram of line angles
+        n_bins = 72
+        range_min = -np.pi
+        range_max = np.pi
+        hist_res = (range_max - range_min) / n_bins # [degrees/bin] resolution of histogram
+        bin_counts, edges = np.histogram(alphas, bins=n_bins, range=(range_min, range_max))
+        # # plot histogram of line angles
+        # fig, axs = plt.subplots()
+        # axs.hist(np.degrees(alphas), bins=n_bins, range=(np.degrees(range_min), np.degrees(range_max)))
+        # plt.show(block=True)
+
+        # evaluate histogram around previous angle of robot to the center line of corn row
+        tol_degrees = 10 # [degrees] when determining alpha, we will look for angles +- this angle around alpha_old
+        tol_idx = np.round(np.radians(tol_degrees) / hist_res)
+        alpha_old = alpha_new
+        alpha_old_hist_idx = np.round((alpha_old + np.pi) / hist_res)
+        if alpha_old_hist_idx + tol_idx >= n_bins:
+            indices = np.r_[(alpha_old_hist_idx - tol_idx):, 0:(alpha_old_hist_idx + tol_idx - n_bins - 1)]
+        elif alpha_old_hist_idx - tol_idx < 0:
+            indices = np.r_[(alpha_old_hist_idx - tol_idx + n_bins):, 0:(alpha_old_hist_idx + tol_idx + 1)]
+        else:
+            indices = np.r_[(alpha_old_hist_idx - tol_idx):(alpha_old_hist_idx + tol_idx + 1)]
+        indices = indices.astype(dtype=int)
+
+        # determine current angle of robot to the center line of corn row
+        bin_counts_peak = bin_counts[indices]
+        edges_peak = edges[indices] + hist_res / 2
+        alpha_new = np.sum(bin_counts_peak * edges_peak) / np.sum(bin_counts_peak)
+
+        np.set_printoptions(precision=1, suppress=True)
+        print("number of plants:", plants_all.shape[0], "angle:", np.degrees(alpha_new))
     else:
         pass
 
@@ -66,8 +128,9 @@ def state_in_row(pub_vel):
     # act_ydist = -p_gain_ydist*error_ydist
     
     cmd_vel = Twist()
-    cmd_vel.linear.x = 0.2
-    cmd_vel.angular.z = act_alpha
+    cmd_vel.linear.x = 0.0
+    cmd_vel.angular.z = 0.0
+    # cmd_vel.angular.z = act_alpha
     # cmd_vel.angular.z = act_alpha/2 + act_ydist/2
 
     pub_vel.publish(cmd_vel)        
@@ -191,7 +254,9 @@ turn_l = np.pi/2
 turn_r = -np.pi/2
 lin_row_exit = 1.0
 lin_row_enter = lin_row_exit
-state = "state_in_row" 
+state = "state_in_row"
+alpha_old = 0.0
+alpha_new = 0.0
 
 if __name__ == '__main__':
     rospy.init_node('path_planning', anonymous=True)
